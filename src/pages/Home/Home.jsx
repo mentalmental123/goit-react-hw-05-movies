12c324a97bcc19aa63a041a9453ce01c80420dac
Олексій Лузанчук <112120213+mentalmental123@users.noreply.github.com>
2023-07-24 19:39:37 +0000

    Update 17 files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+
+import { Link, Outlet } from "react-router-dom";
+
+
+
+
+export const Home = (trendingQuery) => {
+    return(
+      <>
+         <h1>Trending today</h1>
+         <ul>
+          {
+          
+            }
+         </ul>
+      </>
+    )
+}
